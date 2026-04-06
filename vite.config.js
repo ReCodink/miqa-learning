53f9c075959877c9d51761fb444d39b17a475d73
ReCodink <muhammadraihan1607@gmail.com>
2026-04-06 15:58:12 +0700

    feat initial Laravel 12 project setup - Miqa Learning

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/admin.css",
+                "resources/js/admin.js",
+            ],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js"),
+        },
+    },
+    server: {
+        watch: {
+            ignored: ["**/storage/framework/views/**"],
+        },
+    },
+});
